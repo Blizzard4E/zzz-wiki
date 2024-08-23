02b2ef2fde38f7e09eb19db8adf8994895a0241e
Choung Pheng Ear <45393928+Blizzard4E@users.noreply.github.com>
2024-08-23 10:41:10 +0700

    update types

--- composables/types.ts
+++ composables/types.ts
@@ -2,11 +2,23 @@ export type Agent = {
     id: number;
     name: string;
     portrait: string;
+    rank: Rank;
+    specialty: Specialty;
     attribute: Attribute;
     faction: Faction;
 };
 
+export type Rank = {
+    id: number;
+    name: string;
+};
+
 export type Attribute = {
+    id: number;
+    name: string;
+};
+
+export type Specialty = {
     id: number;
     name: string;
 };
